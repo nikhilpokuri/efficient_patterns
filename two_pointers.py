ec95c2b2275760989d4a20e563c39cd9afff33cb
nikhil <nikhilpokuri143@gmail.com>
2024-05-05 00:32:46 +0530

    modified

--- two_pointers.py
+++ two_pointers.py
@@ -1,21 +1,23 @@
 arr = sorted(list(map(int,input("Enter arr Elements: ").split())))
 target = int(input("Enter Target Sum: "))
 
+def target_sum(arr, target):
 #check if there is any pair sum that matches target
-start = 0
-end = len(arr)-1
+    start = 0
+    end = len(arr)-1
 
-res_pairs = []
-while start < end:
-    curr = arr[start] + arr[end]
-    if curr > target:
-        end -= 1
-    elif curr < target:
-        start += 1
+    res_pairs = []
+    while start < end:
+        curr = arr[start] + arr[end]
+        if curr > target:
+            end -= 1
+        elif curr < target:
+            start += 1
+        else:
+            res_pairs.append([arr[start], arr[end]])
+            start += 1
+    if not res_pairs:
+        return 'No Pair sum equals to target'
     else:
-        res_pairs.append([arr[start], arr[end]])
-        start += 1
-if not res_pairs:
-    print(None)
-else:
-    print(res_pairs)
+        return res_pairs
+print(target_sum(arr, target))
